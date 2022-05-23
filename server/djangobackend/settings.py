a422dd8a9c7ef1e932c71de47f8172b3cc3e10b0
lotfy2125 <saylotfy@gmail.com>
2022-05-23 16:23:18 -0400

    adding route to manifest file

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['127.0.0.1','sayed.us-south.cf.appdomain.cloud',]
+ALLOWED_HOSTS = ['127.0.0.1','sayed.us-south.cf.appdomain.cloud/djangoapp']
 
 
 # Application definition
